2ac3694bc8e431d77f4b9b4483ed927dcdb6c8b1
Matheus Lagos <60862073+Matheuslagos@users.noreply.github.com>
2023-11-17 08:26:06 -0300

    Update Comparador.java

--- ArvoreBinariaP/ArvoreBinariaP/src/Comparador.java
+++ ArvoreBinariaP/ArvoreBinariaP/src/Comparador.java
@@ -1,11 +1,24 @@
 public class Comparador {
-    public int comparadorint(int aux1, int aux2) {
-        if (aux1 > aux2) {
+
+    public int comparar(Object obj1, Object obj2) {
+        int chave1 = converterParaInt(obj1);
+        int chave2 = converterParaInt(obj2);
+
+        if (chave1 > chave2) {
             return 1;
-        } else if (aux1 < aux2) {
+        } else if (chave1 < chave2) {
             return -1;
         } else {
             return 0;
+        }
+    }
+
+    private int converterParaInt(Object obj) {
+        if (obj instanceof Integer) {
+            return (int) obj;
+        } else {
+            
+            throw new IllegalArgumentException("A chave não é um Integer.");
         }
     }
 }
