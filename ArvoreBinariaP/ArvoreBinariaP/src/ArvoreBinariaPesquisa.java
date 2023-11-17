6bcbd3d9b4332e1e9cd7027265d938bbaa397e7f
matheus <matheuslagos@hotmail.com.br>
2023-11-16 23:16:21 -0300

    att

--- ArvoreBinariaP/ArvoreBinariaP/src/ArvoreBinariaPesquisa.java
+++ ArvoreBinariaP/ArvoreBinariaP/src/ArvoreBinariaPesquisa.java
@@ -2,34 +2,34 @@ import java.util.Iterator;
 
 class ArvoreBinariaPesquisa implements IArvoreBinariaPesquisa {
     private No raiz;
-    
+
     @Override
     public void setComparator(Comparador c) {
-        // TODO Auto-generated method stub
+
         throw new UnsupportedOperationException("Unimplemented method 'setComparator'");
     }
 
     @Override
     public Comparador getComparador() {
-        // TODO Auto-generated method stub
+
         throw new UnsupportedOperationException("Unimplemented method 'getComparador'");
     }
 
     @Override
     public No pesquisar(No no, Object key) {
-        // TODO Auto-generated method stub
+
         throw new UnsupportedOperationException("Unimplemented method 'pesquisar'");
     }
 
     @Override
     public No incluir(Object key) {
-        // TODO Auto-generated method stub
+
         throw new UnsupportedOperationException("Unimplemented method 'incluir'");
     }
 
     @Override
     public Object remover(Object key) {
-        // TODO Auto-generated method stub
+
         throw new UnsupportedOperationException("Unimplemented method 'remover'");
     }
 
@@ -45,61 +45,61 @@ class ArvoreBinariaPesquisa implements IArvoreBinariaPesquisa {
 
     @Override
     public void emOrdem(No no) {
-        // TODO Auto-generated method stub
+
         throw new UnsupportedOperationException("Unimplemented method 'emOrdem'");
     }
 
     @Override
     public void preOrdem(No no) {
-        // TODO Auto-generated method stub
+
         throw new UnsupportedOperationException("Unimplemented method 'preOrdem'");
     }
 
     @Override
     public void posOrdem(No no) {
-        // TODO Auto-generated method stub
+
         throw new UnsupportedOperationException("Unimplemented method 'posOrdem'");
     }
 
     @Override
     public int altura(No no) {
-        // TODO Auto-generated method stub
+
         throw new UnsupportedOperationException("Unimplemented method 'altura'");
     }
 
     @Override
     public int profundidade(No no) {
-        // TODO Auto-generated method stub
+
         throw new UnsupportedOperationException("Unimplemented method 'profundidade'");
     }
 
     @Override
     public void mostrar() {
-        // TODO Auto-generated method stub
+
         throw new UnsupportedOperationException("Unimplemented method 'mostrar'");
     }
 
     @Override
     public Iterator nos() {
-        // TODO Auto-generated method stub
+
         throw new UnsupportedOperationException("Unimplemented method 'nos'");
     }
 
     @Override
     public Iterator elements() {
-        // TODO Auto-generated method stub
+
         throw new UnsupportedOperationException("Unimplemented method 'elements'");
     }
 
     @Override
     public int size() {
-        // TODO Auto-generated method stub
+
         throw new UnsupportedOperationException("Unimplemented method 'size'");
     }
 
     @Override
     public boolean isEmpty() {
-        // TODO Auto-generated method stub
+
         throw new UnsupportedOperationException("Unimplemented method 'isEmpty'");
     }
 
