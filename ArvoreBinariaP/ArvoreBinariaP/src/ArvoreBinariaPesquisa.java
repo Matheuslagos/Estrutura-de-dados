88587a6de1651f0894708a46b1453ad391a522df
Matheus Lagos <60862073+Matheuslagos@users.noreply.github.com>
2023-11-17 08:24:42 -0300

    Update ArvoreBinariaPesquisa.java

--- ArvoreBinariaP/ArvoreBinariaP/src/ArvoreBinariaPesquisa.java
+++ ArvoreBinariaP/ArvoreBinariaP/src/ArvoreBinariaPesquisa.java
@@ -1,103 +1,112 @@
 import java.util.Iterator;
 
-class ArvoreBinariaPesquisa implements IArvoreBinariaPesquisa {
+class ArvoreBinariaPesquisa {
     private No raiz;
-
-    @Override
+    private Comparador comparador;
+   
     public void setComparator(Comparador c) {
-
-        throw new UnsupportedOperationException("Unimplemented method 'setComparator'");
+        this.comparador = c;
     }
 
-    @Override
     public Comparador getComparador() {
-
-        throw new UnsupportedOperationException("Unimplemented method 'getComparador'");
+        return this.comparador;
     }
-
-    @Override
-    public No pesquisar(No no, Object key) {
-
-        throw new UnsupportedOperationException("Unimplemented method 'pesquisar'");
-    }
-
-    @Override
-    public No incluir(Object key) {
-
-        throw new UnsupportedOperationException("Unimplemented method 'incluir'");
-    }
-
-    @Override
-    public Object remover(Object key) {
-
-        throw new UnsupportedOperationException("Unimplemented method 'remover'");
-    }
-
-    @Override
+   
     public No getRaiz() {
         return this.raiz;
     }
 
-    @Override
+   
     public void setRaiz(No p) {
         this.raiz = p;
     }
 
-    @Override
+    public No pesquisar(No no, Object chave) {
+
+        throw new UnsupportedOperationException("Unimplemented method 'pesquisar'");
+    }
+
+   
+    public No incluir(Object chave) {
+        raiz = inserirRecursivo(raiz, chave);
+        return raiz;
+        
+        
+    }
+    private No inserirRecursivo(No no, Object chave) {
+        if (no == null) {
+            return new No(chave);
+        }
+
+        int comparacao = comparador.comparar(chave, no.getChave());
+
+        if (comparacao < 0) {
+            no.setFilhoEsquerdo(inserirRecursivo(no.getFilhoEsquerdo(), chave));
+            no.getFilhoEsquerdo().setPai(no);
+        } else if (comparacao > 0) {
+            no.setFilhoDireito(inserirRecursivo(no.getFilhoDireito(), chave));
+            no.getFilhoDireito().setPai(no);
+        }
+
+        return no;
+    }
+   
+    public Object remover(Object chave) {
+
+        throw new UnsupportedOperationException("Unimplemented method 'remover'");
+    }
+
     public void emOrdem(No no) {
 
         throw new UnsupportedOperationException("Unimplemented method 'emOrdem'");
     }
-
-    @Override
+  
     public void preOrdem(No no) {
 
         throw new UnsupportedOperationException("Unimplemented method 'preOrdem'");
     }
-
-    @Override
+  
     public void posOrdem(No no) {
 
         throw new UnsupportedOperationException("Unimplemented method 'posOrdem'");
     }
-
-    @Override
+ 
     public int altura(No no) {
 
         throw new UnsupportedOperationException("Unimplemented method 'altura'");
     }
 
-    @Override
+   
     public int profundidade(No no) {
 
         throw new UnsupportedOperationException("Unimplemented method 'profundidade'");
     }
 
-    @Override
+   
     public void mostrar() {
-
+        
         throw new UnsupportedOperationException("Unimplemented method 'mostrar'");
     }
 
-    @Override
+   
     public Iterator nos() {
 
         throw new UnsupportedOperationException("Unimplemented method 'nos'");
     }
 
-    @Override
+   
     public Iterator elements() {
 
         throw new UnsupportedOperationException("Unimplemented method 'elements'");
     }
 
-    @Override
+   
     public int size() {
 
         throw new UnsupportedOperationException("Unimplemented method 'size'");
     }
 
-    @Override
+   
     public boolean isEmpty() {
 
         throw new UnsupportedOperationException("Unimplemented method 'isEmpty'");
