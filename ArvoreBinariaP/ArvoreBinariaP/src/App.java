7414a1bab1c24d54b92a65e0343453ddcad5c51e
matheus <matheuslagos@hotmail.com.br>
2023-11-18 12:58:29 -0300

    att

--- ArvoreBinariaP/ArvoreBinariaP/src/App.java
+++ ArvoreBinariaP/ArvoreBinariaP/src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ArvoreBinariaPesquisa abp = new ArvoreBinariaPesquisa(new Comparador());
+        abp.incluir(10);
+        abp.incluir(5);
+        abp.incluir(15);
+        abp.emOrdem(abp.getRaiz());
+        No noPesquisa = abp.pesquisar(7);
+        if (noPesquisa != null) {
+            System.out.println("Encontrou o nó com chave " + noPesquisa.getChave());
+        }
+        if (noPesquisa == null) {
+            System.out.println("Não encontrou o nó com chave");
+        }
     }
 }
--- ArvoreBinariaP/ArvoreBinariaP/src/ArvoreBinariaPesquisa.java
+++ ArvoreBinariaP/ArvoreBinariaP/src/ArvoreBinariaPesquisa.java
@@ -3,7 +3,11 @@ import java.util.Iterator;
 class ArvoreBinariaPesquisa {
     private No raiz;
     private Comparador comparador;
-   
+
+    public ArvoreBinariaPesquisa(Comparador comparador) {
+        this.comparador = comparador;
+    }
+
     public void setComparator(Comparador c) {
         this.comparador = c;
     }
@@ -11,38 +15,42 @@ class ArvoreBinariaPesquisa {
     public Comparador getComparador() {
         return this.comparador;
     }
-   
+
     public No getRaiz() {
         return this.raiz;
     }
 
-   
     public void setRaiz(No p) {
         this.raiz = p;
     }
 
-    public No pesquisar(No no, Object chave) {
-        int comparacao = comparador.comparar(chave, no.getChave());
-        if (comparacao == 0){
-            return no;
-        }
-        else if(comparacao < 0){
-            return pesquisar(no.getFilhoEsquerdo(),chave);
-        }
-        else{
-            return pesquisar(no.getFilhoDireito(), chave);
-        }
-        
+    /** ----------------------------------------------------------- */
+    public No pesquisar(Object chave) {
+        return pesquisaRecursiva(raiz, chave);
     }
 
-   
-    public No incluir(Object chave) {
-        raiz = inserirRecursivo(raiz, chave);
-        return raiz;
-        
-        
+    private No pesquisaRecursiva(No no, Object chave) {
+        if (no == null || chave == null) {
+            return null;
+        }
+        int comparacao = comparador.comparar(chave, no.getChave());
+        if (comparacao == 0) {
+            return no;
+        } else if (comparacao < 0) {
+            return pesquisaRecursiva(no.getFilhoEsquerdo(), chave);
+        } else {
+            return pesquisaRecursiva(no.getFilhoDireito(), chave);
+        }
+
     }
-    private No inserirRecursivo(No no, Object chave) {
+
+    public No incluir(Object chave) {
+        raiz = incluirRecursivo(raiz, chave);
+        return raiz;
+
+    }
+
+    private No incluirRecursivo(No no, Object chave) {
         if (no == null) {
             return new No(chave);
         }
@@ -50,72 +58,70 @@ class ArvoreBinariaPesquisa {
         int comparacao = comparador.comparar(chave, no.getChave());
 
         if (comparacao < 0) {
-            no.setFilhoEsquerdo(inserirRecursivo(no.getFilhoEsquerdo(), chave));
+            no.setFilhoEsquerdo(incluirRecursivo(no.getFilhoEsquerdo(), chave));
             no.getFilhoEsquerdo().setPai(no);
         } else if (comparacao > 0) {
-            no.setFilhoDireito(inserirRecursivo(no.getFilhoDireito(), chave));
+            no.setFilhoDireito(incluirRecursivo(no.getFilhoDireito(), chave));
             no.getFilhoDireito().setPai(no);
         }
 
         return no;
     }
-   
+
     public Object remover(Object chave) {
 
         throw new UnsupportedOperationException("Unimplemented method 'remover'");
     }
 
     public void emOrdem(No no) {
+        if (no != null) {
+            emOrdem(no.getFilhoEsquerdo());
+            System.out.println(no.getChave());
+            emOrdem(no.getFilhoDireito());
+        }
 
-        throw new UnsupportedOperationException("Unimplemented method 'emOrdem'");
     }
-  
+
     public void preOrdem(No no) {
 
         throw new UnsupportedOperationException("Unimplemented method 'preOrdem'");
     }
-  
+
     public void posOrdem(No no) {
 
         throw new UnsupportedOperationException("Unimplemented method 'posOrdem'");
     }
- 
+
     public int altura(No no) {
 
         throw new UnsupportedOperationException("Unimplemented method 'altura'");
     }
 
-   
     public int profundidade(No no) {
 
         throw new UnsupportedOperationException("Unimplemented method 'profundidade'");
     }
 
-   
     public void mostrar() {
 
         throw new UnsupportedOperationException("Unimplemented method 'mostrar'");
     }
 
-   
     public Iterator nos() {
 
         throw new UnsupportedOperationException("Unimplemented method 'nos'");
     }
 
-   
     public Iterator elements() {
 
         throw new UnsupportedOperationException("Unimplemented method 'elements'");
     }
 
-   
     public int size() {
 
         throw new UnsupportedOperationException("Unimplemented method 'size'");
     }
 
-   
     public boolean isEmpty() {
 
         throw new UnsupportedOperationException("Unimplemented method 'isEmpty'");
