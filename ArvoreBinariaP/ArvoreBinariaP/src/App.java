bbd39421e6d67872aeba5d656dffe2573e3bb218
Matheus Lagos <60862073+Matheuslagos@users.noreply.github.com>
2023-11-28 19:03:55 -0300

    Update App.java

--- ArvoreBinariaP/ArvoreBinariaP/src/App.java
+++ ArvoreBinariaP/ArvoreBinariaP/src/App.java
@@ -1,49 +1,32 @@
-import java.util.Iterator;
 public class App {
-    
     public static void main(String[] args) throws Exception {
         ArvoreBinariaPesquisa abp = new ArvoreBinariaPesquisa(new Comparador());
         abp.incluir(10);
         abp.incluir(5);
         abp.incluir(15);
-        abp.incluir(4);
-        abp.incluir(6);
-        abp.incluir(16);
-        abp.incluir(14);
-        abp.incluir(13);
+        abp.incluir(44);
+        abp.incluir(2);
+        abp.incluir(7);
         abp.incluir(12);
-        
-        System.out.println("Profundidade: "+ abp.profundidade(abp.pesquisar(15)));
-        
-        
-        System.out.println(abp.altura(abp.pesquisar(10)));
-        System.out.println(abp.altura(abp.pesquisar(5)));
-        System.out.println(abp.altura(abp.pesquisar(15)));
-        System.out.println(abp.altura(abp.pesquisar(4)));
-        System.out.println(abp.altura(abp.pesquisar(6)));
+        abp.incluir(20);
+        abp.incluir(1);
 
-        /*System.out.println("Profundidade: "+ abp.profundidade(abp.pesquisar(4)));
-        
-        No noPesquisa = abp.pesquisar(6);
-        if (noPesquisa != null) {
-            System.out.println("Encontrou o nó com chave " + noPesquisa.getChave());
-        }
-        if (noPesquisa == null) {
-            System.out.println("Não encontrou o nó com chave");
-        }*/
-        /*abp.emOrdem(abp.getRaiz());*/
         abp.mostrar();
-        Iterator<No> iteratorNos = abp.nos();
-        while (iteratorNos.hasNext()) {
-        No no = iteratorNos.next();
-        System.out.println("Chave do nó: " + no.getChave());
-        }
+        System.out.println("Removendo 5");
+        abp.remover(5);
+        abp.mostrar();
 
-        // Obter um iterador sobre os valores da árvore
-        Iterator<Object> iteratorElementos = abp.elements();
-        while (iteratorElementos.hasNext()) {
-        Object elemento = iteratorElementos.next();
-        System.out.println("Elemento da árvore: " + elemento);
-        }
+        System.out.println(abp.altura(abp.getRaiz()));
+
+        System.out.println(abp.profundidade(abp.getRaiz()));
+
+        System.out.println("Em ordem");
+        abp.emOrdem(abp.getRaiz());
+        System.out.println("Pré ordem");
+        abp.preOrdem();
+        System.out.println("Pós ordem");
+        abp.posOrdem();
+        System.out.println("Tamanho");
+        System.out.println(abp.size());
     }
 }
